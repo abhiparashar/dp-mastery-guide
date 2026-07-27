2809533057e09bb8c97b6ec0c8d34c362cf323c3
Abhishek Parashar <aparashar103@gmail.com>
2026-07-27 23:55:52 +0530

    1d easy problems

--- 1d dp/easy/ClimbingStairs.java
+++ 1d dp/easy/ClimbingStairs.java
@@ -13,27 +13,34 @@
  */
 public class ClimbingStairs {
 
-    public int climbStairs(int n) {
-        // TODO: implement
-        return 0;
+  public int climbStairs(int n) {
+    if (n <= 1)
+      return 1;
+    int[] dp = new int[n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
     }
+    return dp[n];
+  }
 
-    public static void main(String[] args) {
-        ClimbingStairs sol = new ClimbingStairs();
+  public static void main(String[] args) {
+    ClimbingStairs sol = new ClimbingStairs();
 
-        // Test case 1: n = 2 -> expected 2
-        System.out.println("Test 1: " + sol.climbStairs(2) + " (expected 2)");
+    // Test case 1: n = 2 -> expected 2
+    System.out.println("Test 1: " + sol.climbStairs(2) + " (expected 2)");
 
-        // Test case 2: n = 3 -> expected 3
-        System.out.println("Test 2: " + sol.climbStairs(3) + " (expected 3)");
+    // Test case 2: n = 3 -> expected 3
+    System.out.println("Test 2: " + sol.climbStairs(3) + " (expected 3)");
 
-        // Test case 3: n = 1 -> expected 1
-        System.out.println("Test 3: " + sol.climbStairs(1) + " (expected 1)");
+    // Test case 3: n = 1 -> expected 1
+    System.out.println("Test 3: " + sol.climbStairs(1) + " (expected 1)");
 
-        // Test case 4: n = 5 -> expected 8
-        System.out.println("Test 4: " + sol.climbStairs(5) + " (expected 8)");
+    // Test case 4: n = 5 -> expected 8
+    System.out.println("Test 4: " + sol.climbStairs(5) + " (expected 8)");
 
-        // Test case 5: n = 45 -> expected 1836311903
-        System.out.println("Test 5: " + sol.climbStairs(45) + " (expected 1836311903)");
-    }
+    // Test case 5: n = 45 -> expected 1836311903
+    System.out.println("Test 5: " + sol.climbStairs(45) + " (expected 1836311903)");
+  }
 }
--- 1d dp/easy/MinCostClimbingStairs.java
+++ 1d dp/easy/MinCostClimbingStairs.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * LeetCode 746: Min Cost Climbing Stairs
  * https://leetcode.com/problems/min-cost-climbing-stairs/
@@ -15,29 +17,44 @@
  * 0 <= cost[i] <= 999
  */
 public class MinCostClimbingStairs {
+  // top down memoization
+  public int minCostClimbingStairs(int[] cost) {
+    int n = cost.length;
+    int[] memo = new int[n + 1];
+    Arrays.fill(memo, -1);
+    return helper(cost, memo, n);
+  }
 
-    public int minCostClimbingStairs(int[] cost) {
-        // TODO: implement
-        return 0;
+  private int helper(int[] cost, int[] memo, int n) {
+    if (n <= 1) {
+      return 0;
     }
-
-    public static void main(String[] args) {
-        MinCostClimbingStairs sol = new MinCostClimbingStairs();
-
-        // Test case 1: cost = [10,15,20] -> expected 15
-        int[] cost1 = {10, 15, 20};
-        System.out.println("Test 1: " + sol.minCostClimbingStairs(cost1) + " (expected 15)");
-
-        // Test case 2: cost = [1,100,1,1,1,100,1,1,100,1] -> expected 6
-        int[] cost2 = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
-        System.out.println("Test 2: " + sol.minCostClimbingStairs(cost2) + " (expected 6)");
-
-        // Test case 3: cost = [0,0,0,1] -> expected 0
-        int[] cost3 = {0, 0, 0, 1};
-        System.out.println("Test 3: " + sol.minCostClimbingStairs(cost3) + " (expected 0)");
-
-        // Test case 4: cost = [1,2] -> expected 1
-        int[] cost4 = {1, 2};
-        System.out.println("Test 4: " + sol.minCostClimbingStairs(cost4) + " (expected 1)");
+    if (memo[n] != -1) {
+      return memo[n];
     }
+    int oneStep = helper(cost, memo, n - 1) + cost[n - 1];
+    int twoSteps = helper(cost, memo, n - 2) + cost[n - 2];
+    memo[n] = Math.min(oneStep, twoSteps);
+    return memo[n];
+  }
+
+  public static void main(String[] args) {
+    MinCostClimbingStairs sol = new MinCostClimbingStairs();
+
+    // Test case 1: cost = [10,15,20] -> expected 15
+    int[] cost1 = { 10, 15, 20 };
+    System.out.println("Test 1: " + sol.minCostClimbingStairs(cost1) + " (expected 15)");
+
+    // Test case 2: cost = [1,100,1,1,1,100,1,1,100,1] -> expected 6
+    int[] cost2 = { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+    System.out.println("Test 2: " + sol.minCostClimbingStairs(cost2) + " (expected 6)");
+
+    // Test case 3: cost = [0,0,0,1] -> expected 0
+    int[] cost3 = { 0, 0, 0, 1 };
+    System.out.println("Test 3: " + sol.minCostClimbingStairs(cost3) + " (expected 0)");
+
+    // Test case 4: cost = [1,2] -> expected 1
+    int[] cost4 = { 1, 2 };
+    System.out.println("Test 4: " + sol.minCostClimbingStairs(cost4) + " (expected 1)");
+  }
 }
